34ac215373a682824711fb83ef6aec636e7ccc25
winstonwang <wwang@lcs.on.ca>
2021-12-11 10:07:44 -0500

    Initial Commit

---
+++ TwoHandNumbers/TwoHandNumbersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwoHandNumbersApp.swift
+//  TwoHandNumbers
+//
+//  Created by Winston Wang on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct TwoHandNumbersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
